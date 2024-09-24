3bf469a7185eb3487ab965432d6cd87e2e63f420
hanju <rondo2860@gmail.com>
2024-09-24 16:24:46 +0900

    setting: 프로젝트 기본 설정 및 DB 연결 #1

---
+++ src/test/java/com/board/payment/PaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
